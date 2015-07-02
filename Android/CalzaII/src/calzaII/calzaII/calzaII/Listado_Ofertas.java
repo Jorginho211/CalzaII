0d0c0fd97c65858babcc70cad130518996d3ba58
Jorge Lopez <jorginho@riseup.net>
2015-07-02 13:21:21 +0200

    Add android project

--- Android/CalzaII/src/calzaII/calzaII/calzaII/Listado_Ofertas.java
+++ Android/CalzaII/src/calzaII/calzaII/calzaII/Listado_Ofertas.java
@@ -15,37 +15,39 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Listado_Ofertas extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listado_ofertas);
-		
-		
+
 		ListView listadoOfertas = (ListView) findViewById(R.id.listaOfertas);
-		
-		ArrayList<String> ofertas = new ArrayList<>();
-		
+
+		ArrayList<String> ofertas = new ArrayList<String>();
+
 		try {
-			
-		URL url = new URL("http://proba24.net76.net/ofertas.php");
-		URLConnection con = url.openConnection();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		con.connect();
-		
-		String oferta;
-		
-		while((oferta = reader.readLine()) != null && oferta.length() > 0 ){
-			ofertas.add(oferta);
-		}
-		
-		reader.close();
-		
-		ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, ofertas);
-		
-		listadoOfertas.setAdapter(adaptador);
-		
+
+			URL url = new URL("http://proba24.net76.net/ofertas.php");
+			URLConnection con = url.openConnection();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			con.connect();
+
+			String oferta;
+
+			while ((oferta = reader.readLine()) != null && oferta.length() > 0) {
+				ofertas.add(oferta);
+			}
+
+			reader.close();
+
+			ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+					android.R.layout.simple_list_item_1, android.R.id.text1,
+					ofertas);
+
+			listadoOfertas.setAdapter(adaptador);
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
